bbfe5647efb986a846fa06552157192ee6ad5259
Emily Kingan <emilykingan@emilykingan.local>
2018-01-22 16:37:49 -0800

    First functional test for styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -110,5 +110,18 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied they both go to sleep
 
+        def test_layout_and_styling(self):
+            # Emily goes to the homepage
+            self.browser.get(self.live_server_url)
+            self.browser.set_window_size(1024, 768)
+
+            # She notices the input box is centered
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+            )
+
 
         self.fail('Finish the test!')
