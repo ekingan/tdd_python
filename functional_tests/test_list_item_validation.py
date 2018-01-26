b8bc3068b8dd4614d92b07424f7abcee7f2f4c92
Emily Kingan <emilykingan@emilykingan.local>
2018-01-26 12:54:38 -0800

    Use form in all views

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -8,28 +8,28 @@ class ItemValidationTest(FunctionalTest):
         # Emily goes to the homepage and accidentally tries to submit an empty list item.
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
-        # The homepage refreshes and there is an error message saying that list items cannot be blank
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector(".has-error").text,
-            "You cannot have an empty list item"
-        ))
-        # She tries again to enter text for the item and it works
+
+        # The browser intercepts the request and does not load the list page
+        self.wait_for(lambda: self.browser.find_element_by_css_selector("#id_text:invalid"))
+
+        # She begins to enter text for the item and the error message disappears
         self.get_item_input_box().send_keys("Buy carrots")
+        self.wait_for(lambda: self.browser.find_elements_by_css_selector('#id_text:valid'))
+
+        # And she can submit it successfully
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy carrots")
 
         # Perversly she tries again to submit a blank item
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # She receives a similar warning on the list page
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector(".has-error").text, 
-            "You cannot have an empty list item"
-        ))
+        # Again the browser will not comply
+        self.wait_for_row_in_list_table("1: Buy carrots")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector("#id_text:invalid")) 
 
         # And she can correct it by filling some text in 
         self.get_item_input_box().send_keys("Chop onions")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector("#id_text:valid"))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: Buy carrots")
         self.wait_for_row_in_list_table("2: Chop onions")
-        self.fail('Finish the test!')
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
 import pdb
 
 class HomePageTest(TestCase):
@@ -67,25 +67,41 @@ class ListViewTest(TestCase):
         )
         self.assertRedirects(response, f'/lists/{correct_list.id}/')
 
-    def test_validation_errors_end_up_on_lists_page(self):
+    def post_invalid_input(self):
         list_ = List.objects.create()
-        response = self.client.post(
-            f'/lists/{list_.id}/',
-            data={'text': ''}
-        )
+        return self.client.post(f'/lists/{list_.id}/', data={'text': ''})
+
+    def test_invalid_input_saves_nothing_to_db(self):
+        self.post_invalid_input()
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_invalid_input_renders_list_template(self):
+        response = self.post_invalid_input()
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'list.html')
-        expected_error = "You cannot have an empty list item"
-        self.assertContains(response, expected_error)
+
+    def test_invalid_input_passes_form_to_template(self):
+        response = self.post_invalid_input()
+        self.assertIsInstance(response.context['form'], ItemForm)
+
+    def test_invalid_input_shows_error_on_page(self):
+        response = self.post_invalid_input()
+        self.assertContains(response, EMPTY_ITEM_ERROR)
 
 class NewListTest(TestCase):
 
-    def test_validation_errors_are_sent_back_to_home_page_template(self):
+    def test_for_invalid_input_renders_home_template(self):
         response = self.client.post('/lists/new', data={'text': ''})
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
-        expected_error = "You cannot have an empty list item"
-        self.assertContains(response, expected_error)
+
+    def test_validation_errors_are_shown_on_home_page(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertContains(response, EMPTY_ITEM_ERROR)
+
+    def test_for_invalid_input_passes_form_to_template(self):
+        response = self.client.post('/lists/new', data={'text': ''})
+        self.assertIsInstance(response.context['form'], ItemForm)
 
     def test_invalid_list_items_are_not_saved(self):
         self.client.post('/lists/new', data={'text': ''})
