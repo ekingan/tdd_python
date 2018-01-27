9706dd929ea557d912c15e1b576fc6474fd396cf
Emily Kingan <emilykingan@emilykingan.local>
2018-01-26 17:38:26 -0800

    Add tests for existing list form

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,6 +1,11 @@
 from django.test import TestCase
 from lists.models import Item, List
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+    EMPTY_ITEM_ERROR,
+    DUPLICATE_ITEM_ERROR,
+    ExistingListItemForm,
+    ItemForm
+)
 import pdb
 from unittest import skip
 
@@ -15,6 +20,12 @@ class HomePageTest(TestCase):
         self.assertIsInstance(response.context['form'], ItemForm)
 
 class ListViewTest(TestCase):
+
+    def test_displays_item_form(self):
+        list_ = List.objects.create()
+        response = self.client.get(f'/lists/{list_.id}/')
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
+        self.assertContains(response, 'name="text"')
 
     def test_uses_list_template(self):
         list_ = List.objects.create()
@@ -83,23 +94,21 @@ class ListViewTest(TestCase):
 
     def test_invalid_input_passes_form_to_template(self):
         response = self.post_invalid_input()
-        self.assertIsInstance(response.context['form'], ItemForm)
+        self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
     def test_invalid_input_shows_error_on_page(self):
         response = self.post_invalid_input()
         self.assertContains(response, EMPTY_ITEM_ERROR)
 
-    @skip
     def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
         list1 = List.objects.create()
         item1 = Item.objects.create(list=list1, text="ooh la la")
         response = self.client.post(f'/lists/{list1.id}/', data={'text': 'ooh la la'})
 
-        expected_error = "You already have this item on your list"
+        expected_error = DUPLICATE_ITEM_ERROR
         self.assertContains(response, expected_error)
         self.assertTemplateUsed(response, 'list.html')
         self.assertEqual(Item.objects.all().count(), 1)
-
 
 class NewListTest(TestCase):
 
